963b9f1213c724ef279d5f7d655fde57bb3ab871
Greg Schofield <gregs@indellient.com>
2017-08-16 14:58:45 -0400

    Coroutines and streaming blocking queue added.

--- src/main/kotlin/org/minibatch/MiniBatch.kt
+++ src/main/kotlin/org/minibatch/MiniBatch.kt
@@ -3,19 +3,37 @@ package org.minibatch
 import com.fasterxml.jackson.core.JsonToken
 import com.fasterxml.jackson.core.TreeNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 import java.io.InputStreamReader
+import java.lang.Thread.sleep
+import java.util.Optional
+import java.util.stream.Stream
+import java.util.concurrent.LinkedBlockingQueue
+//import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
 
 fun main(args: Array<String>) {
-
+    val queue = LinkedBlockingQueue<String>()
     val mapper = ObjectMapper()
     val parser = mapper.getFactory().createParser(InputStreamReader(System.`in`, "UTF-8"))
     var token = parser.nextToken()
-    if(token == JsonToken.START_ARRAY) token = parser.nextToken()
+    if (token == JsonToken.START_ARRAY) token = parser.nextToken()
     var str = arrayOf("user", "statuses_count")
-    while (token == JsonToken.START_OBJECT) {
-        var node: TreeNode = mapper.readTree(parser)
-        token = parser.nextToken()
-        //TODO: doWork(node)
+    //val exec = Executors.newFixedThreadPool(2)
+    launch(CommonPool){
+        while (token == JsonToken.START_OBJECT) {
+            var node: TreeNode = mapper.readTree(parser)
+            println("here")
+            delay(3000)
+            queue.add(node.toString())
+            token = parser.nextToken()
+        }
+        queue.add("POISON")
     }
+    Stream.generate { Optional.ofNullable(queue.poll()) }.filter { e -> e.isPresent }.map { e -> e.get() }.forEach { e -> if(e == "POISON") System.exit(0) else println(e) }
+
 }
 
